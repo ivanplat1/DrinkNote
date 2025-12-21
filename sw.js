98c6a58506a558d7564519c169bb717b8a8add9d
ivanplat1 <ivanplat1@users.noreply.github.com>
2025-12-21 18:43:39 +0000

    deploy: a22dace2bbee0123d5f4eaa4c58173e647fba414

--- sw.js
+++ sw.js
@@ -1,10 +1,9 @@
 // Service Worker для DrinkNote PWA
 const CACHE_NAME = 'drinknote-v1';
-const BASE_PATH = self.location.pathname.replace(/\/sw\.js$/, '');
 const urlsToCache = [
-  BASE_PATH + '/',
-  BASE_PATH + '/index.html',
-  BASE_PATH + '/manifest.json'
+  '/',
+  '/index.html',
+  '/manifest.json'
 ];
 
 // Установка Service Worker
@@ -85,7 +84,7 @@ self.addEventListener('fetch', (event) => {
           
           // Если это запрос документа и нет в кэше, возвращаем index.html
           if (event.request.destination === 'document' || event.request.mode === 'navigate') {
-            return caches.match(BASE_PATH + '/index.html');
+            return caches.match('/index.html');
           }
           
           // Для других запросов возвращаем пустой ответ
